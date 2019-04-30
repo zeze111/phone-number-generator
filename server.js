d3a5f285ed478c09ddd7029614ade608d2adcd83
osaze.edo-osagie <osaze.edo-osagie@blackboardinsurance.com>
2019-04-30 22:27:17 +0100

    setup server to use routes and controllers for API endpoints

--- server.js
+++ server.js
@@ -1,25 +1,31 @@
-import express from'express';
-import next from 'next';
+import express from "express";
+import next from "next";
+import bodyParser from "body-parser";
+import routes from "./server/routes";
 
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
 app
   .prepare()
   .then(() => {
-    const server = express()
+    const server = express();
+    server.use(bodyParser.urlencoded({ extended: false }));
+    server.use(bodyParser.json({}));
 
-    server.get('*', (req, res) => {
-      return handle(req, res)
-    })
+    server.get("*", (req, res) => {
+      return handle(req, res);
+    });
+
+    server.use("/telecomms", routes);
 
     server.listen(3000, err => {
-      if (err) throw err
-      console.log('> Ready on http://localhost:3000')
-    })
+      if (err) throw err;
+      console.log("> Ready on http://localhost:3000");
+    });
   })
   .catch(ex => {
-    console.error(ex.stack)
-    process.exit(1)
-  })
+    console.error(ex.stack);
+    process.exit(1);
+  });
--- server/controllers.js
+++ server/controllers.js
@@ -1,5 +1,21 @@
+import { phoneNumbers } from "./storage";
+
 export default class PhoneNumbers {
   static generate(req, res) {
+    if (phoneNumbers.length < 10000) {
+      const number = "310" + Math.floor(Math.random() * 10000000 + 1);
+      const phoneNo = parseInt(number);
 
+      phoneNumbers.push(phoneNo);
+      return res.status(201).json({
+        status: "Successful",
+        number: phoneNo
+      });
+    } else {
+      return res.status(417).json({
+        status: "Unsuccessful",
+        message: "Storage is full, cannot generate numbers"
+      });
+    }
   }
 }
